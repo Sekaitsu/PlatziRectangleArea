5f05ffa709d0d1fab11b5532719e945e7ed7da75
SekaitsuSistecredito <lbernategordillo@gmail.com>
2022-07-18 09:04:34 -0500

    Platzi Rectangle Area

---
+++ rectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace rectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //rectanlge area
+
+            float sideA = 13.5f;
+            float sideB = 23.4f;
+
+            //rectanlge area formula is a*b
+
+            float area = sideA * sideB;
+
+            Console.WriteLine("the rectangle area is: "+area);
+        }
+    }
+}
